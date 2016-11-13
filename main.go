139cf3ddb59dd19d0370fc40513adde9c6cefbac
chksong <chksong@qq.com>
2016-11-13 15:29:46 +0800

    由于设置Cookie的原因 导致crsf 代码有问题

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 
 	"iaskServer/common"
 	"github.com/codegangsta/negroni"
+
+	"github.com/gorilla/csrf"
 )
 
 func hello(w http.ResponseWriter , r* http.Request)  {
@@ -33,6 +35,9 @@ func main() {
 	//	Handler:n ,
 	//}
 
+	crsfKey := []byte("keep-it-secret-keep-it-safe-----")
+	CSRF := csrf.Protect(crsfKey,csrf.Secure(false))
+
 	//添加csrf 保护
-	http.ListenAndServe(common.AppConfig.Server, common.CSRF(n))
+	http.ListenAndServe(common.AppConfig.Server, CSRF(n))
 }
